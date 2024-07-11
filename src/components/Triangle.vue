c90cea3911efad72ec3c24cd08500cad02ea140a
Luz Lopez Marcano <l.lopezmarcano4@gmail.com>
2024-07-11 00:32:18 -0400

    svg caused issues in prod, switching to image

--- src/components/Triangle.vue
+++ src/components/Triangle.vue
@@ -3,20 +3,20 @@
 </script>
 
 <template>
-<svg width="405" height="360" viewBox="0 0 405 360" fill="none" xmlns="http://www.w3.org/2000/svg">
-<path d="M368.66 95L202.5 350.821L36.3396 95H368.66Z" stroke="#FFDEF3" stroke-width="15"/>
-</svg>
+<img src="../images/triangle.png" />
 
 </template>
 
 <style scoped>
 
-svg {
+img {
     position: relative;
-    bottom: 260px;
-    left: 48px;
     overflow: hidden;
     z-index: -1;
+    width: 200px;
+    height: 160px;
+    bottom: 210px;
+    left: 50px;
 }
 
 </style>
